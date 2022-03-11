1c16976f1b4e952222120a5e9841da0da6322afe
Brayan Leyva <brayanleyvag@gmail.com>
2022-03-11 02:26:16 -0600

    cards css added

--- src/components/CharacterInfo.js
+++ src/components/CharacterInfo.js
@@ -14,12 +14,16 @@ export const CharacterInfo = ({cLink}) => {
 
   return (
     <div className='card'>
+        <div>
+          <p><i className='fas fa-circle' style={cInfo.status==='Alive'? {color:'green'} : cInfo.status==='Dead'? {color: 'red'}: {color: 'yellow'}}></i> {cInfo.status}</p>
+        </div>
         <img src={cInfo.image}></img>
         <h5>{cInfo.name}</h5>
-        <p>{cInfo.status}</p>
-        <p>{cInfo.species}</p>
-        <p>{cInfo.origin?.name}</p>
-        <p>{cInfo.episode?.length}</p>
+        <div>
+          <p><b>Species: </b>{cInfo.species}</p>
+          <p><b>Origin: </b>{cInfo.origin?.name}</p>
+          <p><b>Episodes: </b>{cInfo.episode?.length}</p>
+        </div>
     </div>
   )
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { CharacterInfo } from './CharacterInfo'
+import './List.css'
 
 export const List = ({characters}) => {
 
     console.log(characters)
   return (
-    <div>
+    <div className='card-box'>
         {characters?.map((character) =>(
                 <CharacterInfo key={character} cLink={character}/>
         ))}
